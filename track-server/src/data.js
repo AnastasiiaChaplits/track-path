986818ec9dee2744baa9fab4b4ef7760f6bc055a
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2023-03-30 12:11:39 +0300

    Connected to mongo

---
+++ track-server/src/data.js
@@ -0,0 +1,4 @@
+const mongoUri =
+  "mongodb+srv://AnastasiiaChaplits:Password123@cluster0.xgowrv5.mongodb.net/?retryWrites=true&w=majority";
+
+module.exports = { mongoUri };
--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { mongoUri } = require("./data");
 
 const app = express();
+
+mongoose.connect(mongoUri);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there");
