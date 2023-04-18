b182dfeaa02569791bebe68611a15f3ca077fbc7
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2023-04-18 13:29:12 +0300

    Navigation boilerplate

--- tracks/App.js
+++ tracks/App.js
@@ -1,20 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import AccountScreen from "./src/screens/AccountScreen";
+import SignIn from "./src/screens/SignInScreen";
+import SignUp from "./src/screens/SignUpScreen";
+import TrackCreate from "./src/screens/TrackCreateScreen";
+import TrackDetail from "./src/screens/TrackDetailScreen";
+import TrackList from "./src/screens/TrackListScreen";
