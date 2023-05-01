f7c4af8060709bf456c0ed27d4dcd05a4be4f0c7
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2023-05-01 16:09:49 +0300

    Setting tracking the path with the form

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,21 +1,27 @@
 import "../_mockLocation";
-import React, { useContext } from "react";
+import React, { useContext, useCallback } from "react";
 import { StyleSheet, SafeAreaView } from "react-native";
 import { Text } from "react-native-elements";
+import { withNavigationFocus } from "react-navigation";
 import Map from "../components/Map";
 import { Context as LocationContext } from "../context/LocationContext";
 import useLocation from "../hooks/useLocation";
-import { withNavigationFocus } from "react-navigation";
+import TrackForm from "../components/TrackForm";
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { addLocation } = useContext(LocationContext);
-  const [err] = useLocation(isFocused, addLocation);
+  const { state, addLocation } = useContext(LocationContext);
+  const callback = useCallback(
+    (location) => addLocation(location, state.recording),
+    [state.recording]
+  );
+  const [err] = useLocation(isFocused || state.recording, callback);
 
   return (
     <SafeAreaView style={styles.container}>
       <Text h2>Create a track</Text>
       <Map />
       {err ? <Text>Please, enable location services</Text> : null}
+      <TrackForm />
     </SafeAreaView>
   );
 };
