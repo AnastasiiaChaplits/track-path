a450b9634726940f0bcf3fd948850efe118ae05b
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2023-05-01 14:27:16 +0300

    Stop tracking location

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -5,10 +5,11 @@ import { Text } from "react-native-elements";
 import Map from "../components/Map";
 import { Context as LocationContext } from "../context/LocationContext";
 import useLocation from "../hooks/useLocation";
+import { withNavigationFocus } from "react-navigation";
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext);
-  const [err] = useLocation(addLocation);
+  const [err] = useLocation(isFocused, addLocation);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -25,4 +26,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
