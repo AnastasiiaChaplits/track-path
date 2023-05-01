ead241f0837ec732722aa3d5440176dc9eedaa32
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2023-05-01 17:05:08 +0300

    Saving tracks to BE

--- tracks/src/components/TrackForm.js
+++ tracks/src/components/TrackForm.js
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
-import { Input, Button } from "react-native-elements";
+import { Button, Input } from "react-native-elements";
 import Spacer from "./Spacer";
 import { Context as LocationContext } from "../context/LocationContext";
+import useSaveTrack from "../hooks/useSaveTrack";
 
 const TrackForm = () => {
   const {
@@ -10,19 +11,29 @@ const TrackForm = () => {
     stopRecording,
     changeName,
   } = useContext(LocationContext);
-
-  console.log(locations.length);
+  const [saveTrack] = useSaveTrack();
 
   return (
     <>
       <Spacer>
-        <Input placeholder="Enter name" onChange={changeName} value={name} />
+        <Input
+          placeholder="Enter name"
+          onChangeText={changeName}
+          value={name}
+        />
       </Spacer>
-      {recording ? (
-        <Button title="Stop" onPress={stopRecording} />
-      ) : (
-        <Button title="Start recording" onPress={startRecording} />
-      )}
+      <Spacer>
+        {recording ? (
+          <Button title="Stop" onPress={stopRecording} />
+        ) : (
+          <Button title="Start recording" onPress={startRecording} />
+        )}
+      </Spacer>
+      <Spacer>
+        {!recording && locations.length ? (
+          <Button title="Save recording" onPress={saveTrack} />
+        ) : null}
+      </Spacer>
     </>
   );
 };
