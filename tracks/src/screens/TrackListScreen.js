04de6615b6f08dde10f8730f006cffed85f4c4a1
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2023-05-01 17:45:58 +0300

    Styling fixes

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,23 +1,15 @@
 import React, { useContext } from "react";
-import {
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-  SafeAreaView,
-} from "react-native";
+import { StyleSheet, FlatList, TouchableOpacity } from "react-native";
 import { ListItem } from "react-native-elements";
 import { NavigationEvents } from "react-navigation";
-import { Text } from "react-native-elements";
 import { Context as TrackContext } from "../context/TrackContext";
 
 const TrackListScreen = ({ navigation }) => {
   const { state, fetchTracks } = useContext(TrackContext);
 
-  console.log(state);
   return (
-    <SafeAreaView style={styles.container}>
+    <>
       <NavigationEvents onWillFocus={fetchTracks} />
-      <Text h2>Tracks List</Text>
       <FlatList
         data={state}
         keyExtractor={(item) => item._id}
@@ -38,18 +30,14 @@ const TrackListScreen = ({ navigation }) => {
           );
         }}
       />
-    </SafeAreaView>
+    </>
   );
 };
 
 TrackListScreen.navigationOptions = {
-  headerShown: false,
+  title: "Tracks",
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default TrackListScreen;
